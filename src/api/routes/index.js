599f593639e0438c9263c8593460799b322bd1a7
Prathamesh More <pprathameshmore@outlook.com>
2020-08-22 11:09:24 +0530

    Update index.js

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -5,6 +5,6 @@ const userRouter = require("./modules/user");
 const { isAuthenticated } = require("../middlewares/isAuth");
 
 router.use("/v1/auth", authRouter);
-router.use("/v1/user", isAuthenticated, userRouter);
+router.use("/v1/users", isAuthenticated, userRouter);
 
 module.exports = router;
